6442160137113d8fe7fc4d17996cb8c682d4d23a
Jessica Lima <jessikurt@gmail.com>
2023-08-30 09:57:22 -0300

    feat: add product provider for context api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
 import { GlobalStyle } from "../styles/styles";
+import { ProductProvider } from "../context/context-product";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ProductProvider>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ProductProvider>
   )
 }
