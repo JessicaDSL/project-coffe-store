436f5d36d89070d8421890b253b54af12a6a3f45
Jessica Lima <jessikurt@gmail.com>
2023-08-30 09:57:41 -0300

    feat: add product list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Header from "../components/header/header"
+import ProductList from "../components/product-list/product-list"
 
 export default function Page() {
   return (
     <div>
       <Header />
-      <h1>Hello, Next.js!</h1>
+      <ProductList />
     </div>
   )
 }
